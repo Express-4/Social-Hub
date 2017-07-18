6e2316967b45220e5863207112b19aecfe54b1c4
Daniel Georgiev <bgdopus@live.com>
2017-07-19 00:18:20 +0300

    first server running

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ let express = require('express');
 let mongoose = require('mongoose');
 
 let app = express();
+let env = process.env.NODE_ENV || 'development';
+let port = process.env.port || 1234;
+mongoose.Promise = global.Promise;
 
+app.set('view engine', 'pug');
+app.set('views','./server/views');
 app.get('/', (req, res) => {
     mongoose.connect('mongodb://localhost:27017/express4');
 
     console.log('MongoDB ready!');
-    res.send('Cool');
+    res.render('index');
 });
 
-app.listen(1234);
+app.use(express.static('public'))
+
+app.listen(port);
 console.log('Express ready..');
