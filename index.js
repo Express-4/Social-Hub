1da4995e7fa9c9f3af1ae3f4ff8c8b2845295b33
Daniel Georgiev <bgdopus@live.com>
2017-07-19 16:08:23 +0300

    db and express config

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
 let express = require('express');
-let mongoose = require('mongoose');
 
 let app = express();
 let env = process.env.NODE_ENV || 'development';
 let config = require('./server/config/config')[env];
+require('./server/config/database')(config);
+require('./server/config/express')(app, config);
 
-
-mongoose.Promise = global.Promise;
-
-app.set('view engine', 'pug');
-app.set('views','./server/views');
 app.get('/', (req, res) => {
-    mongoose.connect(config.db);
-
-    console.log('MongoDB ready!');
     res.render('index');
 });
 
-app.use(express.static('public'))
-
 app.listen(config.port);
-console.log('Express ready..');
---
+++ server/config/express.js
@@ -0,0 +1,8 @@
+let express = require('express');
+
+module.exports = (app, config) => {
+    app.set('view engine', 'pug');
+    app.set('views', config.rootPath + 'server/views');
+    app.use(express.static(config.rootPath + 'public'));
+    console.log('Express ready!');
+};
