fb627636058e0072406ded079086f8e80bec8e44
Daniel Georgiev <bgdopus@live.com>
2017-07-19 18:44:35 +0300

    working server sceleton

--- server/config/config.js
+++ server/config/config.js
@@ -1,15 +1,16 @@
-const path = require('path');
-let rootPath = path.normalize(path.join(__dirname, '/../../'));
+const path = require('path')
+
+let rootPath = path.normalize(path.join(__dirname, '/../../'))
 
 module.exports = {
-    development:{
-        rootPath: rootPath,
-        db: 'mongodb://localhost:27017/express4',
-        port:  1337
-    },
-    production: {
-        rootPath: rootPath,
-        db: process.env.MONGO_DB_CONN_STRING,
-        port: process.env.port
-    }
-};
+  development: {
+    rootPath: rootPath,
+    db: 'mongodb://localhost:27017/social-hub-db',
+    port: 1337
+  },
+  production: {
+    rootPath: rootPath,
+    db: process.env.MONGO_DB_CONN_STRING,
+    port: process.env.port
+  }
+}
--- server/config/database.js
+++ server/config/database.js
@@ -1,13 +1,21 @@
-const mongoose = require('mongoose')
+let mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
 
 module.exports = (config) => {
-    mongoose.connect(config.db)
-    let db = mongoose.connection
-    db.once('open', (err) => {
-        if (err) throw err
-        console.log('MongoDB ready!')
-    })
-    db.on('error', (err) => console.log('Database error: ' + err))
+  mongoose.connect(config.db)
+
+  let db = mongoose.connection
+
+  db.once('open', err => {
+    if (err) {
+      console.log(err)
+    }
+
+    console.log('MongoDB ready!')
+  })
+
+  db.on('error', err => console.log('Database error: ' + err))
+
+  require('../data/User').seedAdminUser()
 }
