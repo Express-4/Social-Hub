bd9f12827df5cf7dc6ae95471f31824e20cd7ba1
Daniel Georgiev <bgdopus@live.com>
2017-07-19 16:14:44 +0300

    routes config added

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ let env = process.env.NODE_ENV || 'development';
 let config = require('./server/config/config')[env];
 require('./server/config/database')(config);
 require('./server/config/express')(app, config);
-
-app.get('/', (req, res) => {
-    res.render('index');
-});
+require('./server/config/routes')(app)
 
 app.listen(config.port);
