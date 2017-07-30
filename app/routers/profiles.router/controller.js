6ec3d6c9b7c1fd3270e8d502c7d8941d5a179911
Asparuh <puhabg@live.com>
2017-07-30 21:26:11 +0300

    Adds logic to change avatar

--- app/routers/profiles.router/controller.js
+++ app/routers/profiles.router/controller.js
@@ -1,4 +1,6 @@
 const userModel = require('../../../models/user.model');
+const path = require('path');
+const Jimp = require('jimp');
 
 class ProfileController {
     constructor(data) {
@@ -18,6 +20,36 @@ class ProfileController {
             })
             .catch((err) => {
                 res.render('404');
+            });
+    }
+
+    getChangeAvatar(req, res) {
+        res.render('profiles/upload-avatar');
+    }
+
+    saveChangedAvatar(req, res) {
+        const fileName = req.body.fileName.replace(/^.*[\\\/]/, '');
+        const left = req.body.l.replace('-', '').replace('px', '');
+        const top = req.body.t.replace('-', '').replace('px', '');
+        const w = req.body.w.replace('-', '').replace('px', '');
+        const h = req.body.h.replace('-', '').replace('px', '');
+
+        const topCor = parseInt(top, 10);
+        const leftCor = parseInt(left, 10);
+        const height = parseInt(h, 10);
+        const realImagePath = path.join(__dirname, '../../../static/temp/' + fileName);
+        const pathToSave = path.join(__dirname, '../../../static/avatars/' + fileName);
+
+        Jimp.read(realImagePath)
+            .then((image) => {
+                image.crop(leftCor, topCor, height, height)
+                    .write(pathToSave);
+            })
+            .then(() => {
+                const user = req.user;
+                user.avatarPath = pathToSave;
+                this.data.users.updateById(user);
+                res.json({ success: true, avatarFileLocation: pathToSave });
             });
     }
 }
--- app/routers/profiles.router/router.js
+++ app/routers/profiles.router/router.js
@@ -1,4 +1,19 @@
 const { Router } = require('express');
+const path = require('path');
+const multer = require('multer');
+let fileName;
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, '../../../static/temp/'));
+    },
+    filename: (req, file, cb) => {
+        fileName = Date.now() + path.extname(file.originalname);
+        cb(null, fileName);
+    },
+});
+
+const upload = multer({ storage: storage });
 
 const attachTo = (app, data) => {
     const router = new Router();
@@ -14,6 +29,27 @@ const attachTo = (app, data) => {
             }
 
             return controller.getProfile(req, res);
+        })
+        .get('/change/avatar', (req, res) => {
+            if (!req.user) {
+                return Promise.resolve()
+                    .then(() => {
+                        res.redirect('/auth/sign-in');
+                    });
+            }
+
+            return controller.getChangeAvatar(req, res);
+        })
+        .post('/change/avatar', upload.single('file'), (req, res) => {
+            if (req.file) {
+                res.json({
+                    success: true,
+                    fileName: '/static/temp/' + fileName,
+                });
+            }
+        })
+        .post('/avatar/save', (req, res) => {
+            return controller.saveChangedAvatar(req, res);
         });
 
     app.use('/profile', router);
