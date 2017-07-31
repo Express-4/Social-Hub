f553fb159d53af97a6b520203b9d7d5108f480c5
Asparuh <puhabg@live.com>
2017-07-31 13:20:58 +0300

    Deleted not needed files

--- data/data.js
+++ data/data.js
@@ -1,13 +1,9 @@
-const ItemsData = require('./items.data');
 const CategoriesData = require('./categories.data');
-const TodosData = require('./todos.data');
 const UsersData = require('./users.data');
 const PostsData = require('./posts.data');
 
 const init = (db) => {
     return Promise.resolve({
-        items: new ItemsData(db),
-        todos: new TodosData(db),
         categories: new CategoriesData(db),
         users: new UsersData(db),
         posts: new PostsData(db),
